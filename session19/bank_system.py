42aa6df57f6b540f5686a6df61df5fcf3c6742cb
sali mft <thesalimft@gmail.com>
2025-12-10 17:24:40 +0330

    add session 19

--- session19/bank_system.py
+++ session19/bank_system.py
@@ -1,14 +1,53 @@
-class BankSystem:
-    def __init__(self, bankdata):
-        self.bankdata = bankdata
-    
-    def create(self):
-        pass
- 
+from datetime import datetime;
 
-   
+class BankSystem:
+    def __init__(self, accounts):
+        self.accounts = accounts
+    
+    def create_account(self, name:str, balance:float) -> bool:
+        id = len(self.accounts) + 1
+        current_time = datetime.now().strftime('%d %b %Y at %H:%M')
+        self.accounts[str(id)] = {
+            'name': name,
+            'balance': balance,
+            'history': [f'Created on {current_time} with balance = ${balance}']
+        }
+        return True
+
+    def deposit(self, id:str, amount:float) -> bool:
+        if id not in self.accounts:
+            return False
         
-# bankdata = {
+        self.accounts[id]['balance'] += amount
+        
+        
+        current_time = datetime.now().strftime('%d %b %Y at %H:%M')
+        txt = f'Deposit on {current_time} with balance = ${self.accounts[id]['balance']}'
+        self.accounts[id]['history'].append(txt)
+        return True
+        
+    def withdraw(self, id:str, amount:float) -> bool:
+        if id not in self.accounts:
+            return False
+        
+        if self.accounts[id]['balance'] < amount:
+            return False
+
+        self.accounts[id]['balance'] -= amount
+        current_time = datetime.now().strftime('%d %b %Y at %H:%M')
+        txt = f'Withdraw on {current_time} with balance = ${self.accounts[id]['balance']}'
+        self.accounts[id]['history'].append(txt)
+        return True
+        
+        
+       
+       
+ 
+  
+       
+       
+        
+# account = {
 #     '101' : {
 #         'name': 'ali',
 #         'balance': 100,
@@ -17,6 +56,5 @@ class BankSystem:
 #                     {'type': 'deposit', 'time': '14-04-2025 13:41', 'amount': 20, 'remaining': 120},
 #                     {'type': 'withdraw', 'time': '14-04-2025 13:41', 'amount': 10, 'remaining': 110}
 #         ]
-
 #     },
 # }
--- session19/main.py
+++ session19/main.py
@@ -1,11 +1,24 @@
 from bank_system import BankSystem
 
-bankdata = {}
+accounts = dict()
 
 def main():
-    bs = BankSystem(bankdata)
+    bs = BankSystem(accounts)
+    
+    bs.create_account('ali', 100)
+    bs.create_account('reza', 200)
+    print(accounts)
+    
+    bs.deposit('1', 50)
+    print(accounts)
+    
+    bs.withdraw('2', 20)
+    print(accounts)
 
 
 
 
 main()
+
+
+
